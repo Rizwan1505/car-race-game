6ba89dc43b187adda5ccc53df382701314cdb808
mehmetmertyildiran <mehmetmertyildiran@gmail.com>
2014-07-12 23:09:29 +0300

    GUI - loading screen added.

--- Assets/Extra/User Interface/MainMenu.cs
+++ Assets/Extra/User Interface/MainMenu.cs
@@ -10,22 +10,49 @@ public class MainMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (GameObject.Find ("Directional light - Main Menu").light.intensity < 0.5f) {
+			GameObject.Find ("Directional light - Main Menu").light.intensity = GameObject.Find ("Directional light - Main Menu").light.intensity + 0.001f;
+		}
 	}
 
+	public bool GUIEnabled = true;
 	void OnGUI () {
-		// Make a background box
-		GUI.Box(new Rect(10,10,200,300), "Tournament: Formula 1");
+		if (GUIEnabled) {
+				// Make a background box
+				GUI.Box (new Rect (10, 10, 200, 300), "Tournament: Formula 1");
 		
-		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(40,40,140,40), "Find Match")) {
-			Application.LoadLevel(1);
-		}
+				// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+				if (GUI.Button (new Rect (40, 40, 140, 40), "Find Match")) {
+						StartCoroutine (LoadLevel1 ());
+						GameObject.Find ("Main Camera - Main Menu").transform.Rotate (0, 90, 0);
+						GUIEnabled = false;
+				}
 		
-		// Make the second button.
-		if(GUI.Button(new Rect(40,90,140,40), "Practice")) {
-			Application.LoadLevel(2);
+				// Make the second button.
+				if (GUI.Button (new Rect (40, 90, 140, 40), "Practice")) {
+						StartCoroutine (LoadLevel2 ());
+						GameObject.Find ("Main Camera - Main Menu").transform.Rotate (0, 90, 0);
+						GUIEnabled = false;
+				}
 		}
 	}
-	
+
+	void Awake () {
+		GameObject.Find ("Directional light - Main Menu").light.intensity = 0;
+	}
+
+	IEnumerator LoadLevel1() {
+		AsyncOperation async = Application.LoadLevelAsync(1);
+		yield return async;
+		Debug.Log("Loading complete");
+	}
+
+	IEnumerator LoadLevel2() {
+		AsyncOperation async = Application.LoadLevelAsync(2);
+		yield return async;
+		Debug.Log("Loading complete");
+	}
+
+
+
 }
