0a4ed9cd1a491ad68d8ce0f33543922c5dff6f84
mehmetmertyildiran <mehmetmertyildiran@gmail.com>
2014-07-12 21:23:47 +0300

    GUI - main menu improvement.

---
+++ Assets/Extra/User Interface/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		// Make a background box
+		GUI.Box(new Rect(10,10,200,300), "Tournament: Formula 1");
+		
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(40,40,140,40), "Find Match")) {
+			Application.LoadLevel(1);
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(40,90,140,40), "Practice")) {
+			Application.LoadLevel(2);
+		}
+	}
+	
+}
